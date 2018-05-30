55113a1fd83cf70475ea139401938b5da7f854f8
Paul P <paulpatarinski@gmail.com>
2018-05-30 08:27:07 -0500

    Update stencil config with recommended values

--- stencil.config.js
+++ stencil.config.js
@@ -1,8 +1,11 @@
 exports.config = {
   namespace: 'emoji-mart',
-  generateDistribution: true,
   bundles: [
     { components: ['emart-emoji', 'emart-anchors', 'emart-picker', 'emart-search', 'emart-category', 'emart-preview'] }
+  ],
+  outputTargets: [
+    { type: 'www' },
+    { type: 'dist' }
   ]
 };
 
