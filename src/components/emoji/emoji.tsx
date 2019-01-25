5693052c69efc93d6813b726c5a72d27ad9d3368
ppatarinski <ppatarinski@paylocity.com>
2019-01-25 17:25:22 -0600

    Fix an issue with Android devices where emojis appear overlapped

--- src/components/emoji/emoji.tsx
+++ src/components/emoji/emoji.tsx
@@ -2,196 +2,198 @@ import { Component, Prop } from '@stencil/core';
 import { getData, unifiedToNative, getSanitizedData } from 'emoji-mart/dist-es/utils';
 
 @Component({
-    tag: 'emart-emoji',
-    styleUrl: '../../../node_modules/emoji-mart/css/emoji-mart.css' 
+  tag: 'emart-emoji',
+  styleUrl: '../../../node_modules/emoji-mart/css/emoji-mart.css'
 })
 
 export class Emoji {
-    EMOJI_DATASOURCE_VERSION = "4.0.2";
-    SHEET_COLUMNS = 52;
+  EMOJI_DATASOURCE_VERSION = "4.0.2";
+  SHEET_COLUMNS = 52;
 
-    // @Prop() children: any; 
-    @Prop() skin: any = 1;
-    @Prop() set: any = 'apple';
-    @Prop() sheetSize: any = 64;
-    @Prop() native: any = false;
-    @Prop() forceSize: any = false;
-    @Prop() tooltip: any = false;
-    @Prop() backgroundImageFn: any = (set, sheetSize) =>
-        `https://unpkg.com/emoji-datasource-${set}@${this.EMOJI_DATASOURCE_VERSION}/img/${set}/sheets-256/${sheetSize}.png`;
+  // @Prop() children: any; 
+  @Prop() skin: any = 1;
+  @Prop() set: any = 'apple';
+  @Prop() sheetSize: any = 64;
+  @Prop() native: any = false;
+  @Prop() forceSize: any = false;
+  @Prop() tooltip: any = false;
+  @Prop() backgroundImageFn: any = (set, sheetSize) =>
+    `https://unpkg.com/emoji-datasource-${set}@${this.EMOJI_DATASOURCE_VERSION}/img/${set}/sheets-256/${sheetSize}.png`;
 
-    @Prop() onLoaded: any = () => { };
-    @Prop() onClick: any;
-    @Prop() onOver: any;
-    @Prop() onLeave: any;
-    @Prop() size: number;
-    @Prop() fallback: any;
-    @Prop() html: any;
-    @Prop() emoji: any;
+  @Prop() onLoaded: any = () => { };
+  @Prop() onClick: any;
+  @Prop() onOver: any;
+  @Prop() onLeave: any;
+  @Prop() size: number;
+  @Prop() fallback: any;
+  @Prop() html: any;
+  @Prop() emoji: any;
 
-    componentDidLoad() {
-        this._detectImgLoaded(this.backgroundImageFn(this.set,
-            this.sheetSize));
+  componentDidLoad() {
+    this._detectImgLoaded(this.backgroundImageFn(this.set,
+      this.sheetSize));
+  }
+
+  _getPosition = props => {
+    var { sheet_x, sheet_y } = this._getData(props),
+      multiply = 100 / (this.SHEET_COLUMNS - 1)
+
+    return `${multiply * sheet_x}% ${multiply * sheet_y}%`
+  }
+
+  _getData = props => {
+    var { emoji, skin, set } = props;
+    return getData(emoji, skin, set)
+  }
+
+  _getSanitizedData = props => {
+    var { emoji, skin, set } = props
+    return getSanitizedData(emoji, skin, set)
+  }
+
+  _handleClick = (e, props) => {
+    if (!props.onClick) {
+      return
+    }
+    var { onClick } = props,
+      emoji = this._getSanitizedData(props)
+
+    onClick(emoji, e)
+  }
+
+  _handleOver = (e, props) => {
+    if (!props.onOver) {
+      return
+    }
+    var { onOver } = props,
+      emoji = this._getSanitizedData(props)
+
+    onOver(emoji, e)
+  }
+
+  _handleLeave = (e, props) => {
+    if (!props.onLeave) {
+      return
+    }
+    var { onLeave } = props,
+      emoji = this._getSanitizedData(props)
+
+    onLeave(emoji, e)
+  }
+
+  _isNumeric = value => {
+    return !isNaN(value - parseFloat(value))
+  }
+
+  _convertStyleToCSS = style => {
+    let div = document.createElement('div')
+
+    for (let key in style) {
+      let value = style[key]
+
+      if (this._isNumeric(value)) {
+        value += 'px'
+      }
+
+      div.style[key] = value
     }
 
-    _getPosition = props => {
-        var { sheet_x, sheet_y } = this._getData(props),
-            multiply = 100 / (this.SHEET_COLUMNS - 1)
+    return div.getAttribute('style')
+  }
 
-        return `${multiply * sheet_x}% ${multiply * sheet_y}%`
+  _detectImgLoaded = imgSrc => {
+    var image = new Image();
+    image.src = imgSrc;
+    image.onload = () => {
+      this.onLoaded();
+    }
+  }
+
+  render() {
+    let data = this._getData(this)
+
+    if (!data) {
+      return null
     }
 
-    _getData = props => {
-        var { emoji, skin, set } = props;
-        return getData(emoji, skin, set)
+    let style: any = {};
+    let { unified, custom, short_names, imageUrl } = data,
+      children = '',
+      className = 'emoji-mart-emoji',
+      title = null
+
+    if (!unified && !custom) {
+      return null
     }
 
-    _getSanitizedData = props => {
-        var { emoji, skin, set } = props
-        return getSanitizedData(emoji, skin, set)
+    if (this.tooltip) {
+      title = short_names[0]
     }
 
-    _handleClick = (e, props) => {
-        if (!props.onClick) {
-            return
-        }
-        var { onClick } = props,
-            emoji = this._getSanitizedData(props)
+    if (this.native && unified) {
+      className += ' emoji-mart-emoji-native'
+      style = { fontSize: this.size }
+      children = unifiedToNative(unified)
 
-        onClick(emoji, e)
-    }
+      if (this.forceSize) {
+        style.display = 'inline-block'
+        style.width = this.size
+        style.height = this.size
+      }
+    } else if (custom) {
+      className += ' emoji-mart-emoji-custom'
+      style = {
+        width: this.size,
+        height: this.size,
+        display: 'inline-block',
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: 'contain',
+        "-webkit-background-size": 'contain'
+      }
+    } else {
+      let setHasEmoji = this._getData(this)[`has_img_${this.set}`]
 
-    _handleOver = (e, props) => {
-        if (!props.onOver) {
-            return
-        }
-        var { onOver } = props,
-            emoji = this._getSanitizedData(props)
-
-        onOver(emoji, e)
-    }
-
-    _handleLeave = (e, props) => {
-        if (!props.onLeave) {
-            return
-        }
-        var { onLeave } = props,
-            emoji = this._getSanitizedData(props)
-
-        onLeave(emoji, e)
-    }
-
-    _isNumeric = value => {
-        return !isNaN(value - parseFloat(value))
-    }
-
-    _convertStyleToCSS = style => {
-        let div = document.createElement('div')
-
-        for (let key in style) {
-            let value = style[key]
-
-            if (this._isNumeric(value)) {
-                value += 'px'
-            }
-
-            div.style[key] = value
-        }
-
-        return div.getAttribute('style')
-    }
-
-    _detectImgLoaded = imgSrc => {
-        var image = new Image();
-        image.src = imgSrc;
-        image.onload = () => {
-            this.onLoaded();
-        }
-    }
-
-    render() {
-        let data = this._getData(this)
-
-        if (!data) {
-            return null
-        }
-
-        let style: any = {};
-        let { unified, custom, short_names, imageUrl } = data,
-            children = '',
-            className = 'emoji-mart-emoji',
-            title = null
-
-        if (!unified && !custom) {
-            return null
-        }
-
-        if (this.tooltip) {
-            title = short_names[0]
-        }
-
-        if (this.native && unified) {
-            className += ' emoji-mart-emoji-native'
-            style = { fontSize: this.size }
-            children = unifiedToNative(unified)
-
-            if (this.forceSize) {
-                style.display = 'inline-block'
-                style.width = this.size
-                style.height = this.size
-            }
-        } else if (custom) {
-            className += ' emoji-mart-emoji-custom'
-            style = {
-                width: this.size,
-                height: this.size,
-                display: 'inline-block',
-                backgroundImage: `url(${imageUrl})`,
-                backgroundSize: 'contain',
-            }
+      if (!setHasEmoji) {
+        if (this.fallback) {
+          style = { fontSize: this.size }
+          children = this.fallback(data)
         } else {
-            let setHasEmoji = this._getData(this)[`has_img_${this.set}`]
-
-            if (!setHasEmoji) {
-                if (this.fallback) {
-                    style = { fontSize: this.size }
-                    children = this.fallback(data)
-                } else {
-                    return null
-                }
-            } else {
-                style = {
-                    width: `${this.size}px`,
-                    height: `${this.size}px`,
-                    display: 'inline-block',
-                    backgroundImage: `url(${this.backgroundImageFn(
-                        this.set,
-                        this.sheetSize
-                    )})`,
-                    backgroundSize: `${100 * this.SHEET_COLUMNS}%`,
-                    backgroundPosition: this._getPosition(this),
-                }
-            }
+          return null
         }
-
-        if (this.html) {
-            style = this._convertStyleToCSS(style)
-
-            return `<span style='${style}' ${title
-                ? `title='${title}'`
-                : ''} class='${className}'>${children || ''}</span>`
-        } else {
-            return (
-                <span
-                    onClick={e => this._handleClick(e, this)}
-                    onMouseEnter={e => this._handleOver(e, this)}
-                    onMouseLeave={e => this._handleLeave(e, this)}
-                    title={title}
-                    class={className}
-                >
-                    <span style={style}>{children}</span>
-                </span>
-            )
+      } else {
+        style = {
+          width: `${this.size}px`,
+          height: `${this.size}px`,
+          display: 'inline-block',
+          backgroundImage: `url(${this.backgroundImageFn(
+            this.set,
+            this.sheetSize
+          )})`,
+          backgroundSize: `${100 * this.SHEET_COLUMNS}%`,
+          "-webkit-background-size": `${100 * this.SHEET_COLUMNS}%`,
+          backgroundPosition: this._getPosition(this),
         }
+      }
     }
+
+    if (this.html) {
+      style = this._convertStyleToCSS(style)
+
+      return `<span style='${style}' ${title
+        ? `title='${title}'`
+        : ''} class='${className}'>${children || ''}</span>`
+    } else {
+      return (
+        <span
+          onClick={e => this._handleClick(e, this)}
+          onMouseEnter={e => this._handleOver(e, this)}
+          onMouseLeave={e => this._handleLeave(e, this)}
+          title={title}
+          class={className}
+        >
+          <span style={style}>{children}</span>
+        </span>
+      )
+    }
+  }
 }
